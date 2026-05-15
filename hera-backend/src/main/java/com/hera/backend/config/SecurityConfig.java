ce197bf1d097f4f6de605e4daa8582a915a01e03
Christopher Ramírez <christopher_gomez0306@hotmail.com>
2026-05-15 10:22:53 -0600

    fix: Se integraron los estilos de bootstrap en SecurityConfig

--- hera-backend/src/main/java/com/hera/backend/config/SecurityConfig.java
+++ hera-backend/src/main/java/com/hera/backend/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
                         .contentSecurityPolicy(csp -> csp
                                 .policyDirectives("default-src 'self'; " +
                                         "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com; " +
-                                        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+                                        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
                                         "img-src 'self' data: https://res.cloudinary.com; " +
                                         "font-src 'self' https://fonts.gstatic.com;")
                         )
